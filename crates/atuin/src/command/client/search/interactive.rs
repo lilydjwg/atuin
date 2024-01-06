48290ee898cd31bd0ac8127d8151e6f2437c22a3
lilydjwg <lilydjwg@gmail.com>
2024-01-06 16:05:22 +0800

    fix(ui): use Ctrl-k / Ctrl-u to kill to end / start

--- crates/atuin/src/command/client/search/interactive.rs
+++ crates/atuin/src/command/client/search/interactive.rs
@@ -471,7 +471,8 @@ impl State {
                     self.search.input.remove();
                 }
             }
-            KeyCode::Char('u') if ctrl => self.search.input.clear(),
+            KeyCode::Char('u') if ctrl => self.search.input.clear_to_start(),
+            KeyCode::Char('k') if ctrl => self.search.input.clear_to_end(),
             KeyCode::Char('r') if ctrl => self.search.rotate_filter_mode(settings, 1),
             KeyCode::Char('s') if ctrl => {
                 self.switched_search_mode = true;
@@ -487,7 +488,7 @@ impl State {
             KeyCode::Char('n' | 'j') if ctrl => {
                 return self.handle_search_down(settings, false);
             }
-            KeyCode::Char('p' | 'k') if ctrl => {
+            KeyCode::Char('p') if ctrl => {
                 return self.handle_search_up(settings, false);
             }
             KeyCode::Char('l') if ctrl => {
