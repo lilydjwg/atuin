b5a692c42318d9c315c13b5ae397b367b994d13d
lilydjwg <lilydjwg@gmail.com>
2024-12-28 11:11:57 +0800

    fallback to non-daemon if daemon operation fails
    This helps when entering another environment that doesn't have the
    daemon available, e.g. chroot, nsenter, nspawn a virtual machine rootfs.

--- crates/atuin/src/command/client/history.rs
+++ crates/atuin/src/command/client/history.rs
@@ -582,7 +582,9 @@ pub(super) async fn start_history_entry(
 ) -> Result<Option<String>> {
     #[cfg(feature = "daemon")]
     if settings.daemon.enabled {
-        return handle_daemon_start(settings, command, author, intent).await;
+        if let Ok(id) = handle_daemon_start(settings, command, author, intent).await {
+            return Ok(id);
+        }
     }
 
     let db_path = &settings.db_path;
@@ -598,7 +600,9 @@ pub(super) async fn end_history_entry(
 ) -> Result<()> {
     #[cfg(feature = "daemon")]
     if settings.daemon.enabled {
-        return handle_daemon_end(settings, id, exit, duration).await;
+        if handle_daemon_end(settings, id, exit, duration).await.is_ok() {
+            return Ok(());
+        }
     }
 
     let db_path = &settings.db_path;
