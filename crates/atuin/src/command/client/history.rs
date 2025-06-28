5211b44544fb4b3f32d3799eec24234fd5cb3c76
lilydjwg <lilydjwg@gmail.com>
2025-06-28 14:20:50 +0800

    feat(history): add a delete-by-ids command

--- crates/atuin/src/command/client/history.rs
+++ crates/atuin/src/command/client/history.rs
@@ -1,6 +1,6 @@
 use std::{
     fmt::{self, Display},
-    io::{self, IsTerminal, Write},
+    io::{self, IsTerminal, Result as IoResult, Write},
     path::PathBuf,
     time::Duration,
 };
@@ -23,7 +23,7 @@ use atuin_daemon::history::{HistoryEventKind, TailHistoryReply};
 use atuin_client::{
     database::{Database, Sqlite, current_context},
     encryption,
-    history::{History, store::HistoryStore},
+    history::{History, HistoryId, store::HistoryStore},
     record::sqlite_store::SqliteStore,
     settings::{
         FilterMode::{Directory, Global, Session},
@@ -159,6 +159,13 @@ pub enum Cmd {
         /// How many recent duplicates to keep
         #[arg(long)]
         dupkeep: u32,
+    },
+
+    /// Delete history entries matching ids given from stdin
+    DeleteByIds {
+        /// List matching history lines without performing the actual deletion.
+        #[arg(short = 'n', long)]
+        dry_run: bool,
     },
 }
 
@@ -1104,6 +1111,47 @@ impl Cmd {
         Ok(())
     }
 
+    async fn handle_delete_by_ids(
+        db: &impl Database,
+        settings: &Settings,
+        store: SqliteStore,
+        dry_run: bool,
+    ) -> Result<()> {
+        let ids = std::io::stdin()
+            .lines()
+            .map(|s| s.map(HistoryId))
+            .collect::<IoResult<Vec<_>>>()?;
+        let matches: Vec<History> = db.get_by_ids(&ids).await?;
+
+        if dry_run {
+            print_list(
+                &matches,
+                ListMode::Human,
+                Some(settings.history_format.as_str()),
+                false,
+                false,
+                settings.timezone,
+            );
+        } else {
+            let encryption_key: [u8; 32] = encryption::load_key(settings)
+                .context("could not load encryption key")?
+                .into();
+            let host_id = Settings::host_id().await?;
+            let history_store = HistoryStore::new(store.clone(), host_id, encryption_key);
+
+            for entry in matches {
+                eprintln!("deleting {}", entry.id);
+                if settings.sync.records {
+                    let (id, _) = history_store.delete(entry.id).await?;
+                    history_store.incremental_build(db, &[id]).await?;
+                } else {
+                    db.delete(entry).await?;
+                }
+            }
+        }
+        Ok(())
+    }
+
     #[allow(clippy::too_many_lines)]
     pub async fn run(self, settings: &Settings) -> Result<()> {
         match self {
@@ -1220,6 +1268,10 @@ impl Cmd {
                             .unix_timestamp_nanos(),
                         )?;
                         Self::handle_dedup(&db, settings, store, before, dupkeep, dry_run).await
+                    }
+
+                    Self::DeleteByIds { dry_run } => {
+                        Self::handle_delete_by_ids(&db, settings, store, dry_run).await
                     }
 
                     Self::Start { .. } | Self::End { .. } | Self::Tail => unreachable!(),
