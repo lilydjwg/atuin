6d3c9770499b673cc9b4854dc2c6b3ab599a8698
lilydjwg <lilydjwg@gmail.com>
2025-08-23 10:44:09 +0800

    change highlight text color to match changed highlighted row color

--- crates/atuin/src/command/client/search/history_list.rs
+++ crates/atuin/src/command/client/search/history_list.rs
@@ -329,7 +329,8 @@ impl DrawState<'_> {
             let mut char_style = style;
             if highlighted {
                 if row_highlighted {
-                    char_style = self.theme.as_style(Meaning::AlertWarn);
+                    use crossterm::style::Stylize;
+                    char_style = style::ContentStyle::new().with(style::Color::DarkCyan);
                 }
                 char_style.attributes.set(style::Attribute::Bold);
             }
