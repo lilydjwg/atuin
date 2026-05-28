619a9098e1739996892d3638844b5e2ea3995f2a
lilydjwg <lilydjwg@gmail.com>
2026-05-28 13:27:40 +0800

    make preview color lighter

--- crates/atuin/src/command/client/search/interactive.rs
+++ crates/atuin/src/command/client/search/interactive.rs
@@ -1266,6 +1266,7 @@ impl State {
             lines.join("\n")
         };
 
+        use ratatui::crossterm::style::{self, Stylize};
         match compactness {
             Compactness::Full => Paragraph::new(command).block(
                 Block::default()
@@ -1274,7 +1275,7 @@ impl State {
                     .title(format!("{:─>width$}", "", width = chunk_width - 2)),
             ),
             _ => Paragraph::new(command)
-                .style(Style::from_crossterm(theme.as_style(Meaning::Annotation))),
+                .style(Style::from_crossterm(style::ContentStyle::new().with(style::Color::Grey))),
         }
     }
 }
