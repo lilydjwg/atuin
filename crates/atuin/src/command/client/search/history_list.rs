acb38683bb8f0b7668bded596a3b2f3368ded19e
lilydjwg <lilydjwg@gmail.com>
2026-07-20 17:09:47 +0800

    highlight … as grey to make it clear

--- crates/atuin/src/command/client/search/history_list.rs
+++ crates/atuin/src/command/client/search/history_list.rs
@@ -348,6 +348,9 @@ impl DrawState<'_> {
                     char_style = style::ContentStyle::new().with(style::Color::DarkCyan);
                 }
                 char_style.attributes.set(style::Attribute::Bold);
+            } else if ellipsized.source_index(i).is_none() {
+                use crossterm::style::Stylize;
+                char_style = style::ContentStyle::new().with(style::Color::DarkGrey);
             }
             self.draw(&ch.to_string(), Style::from_crossterm(char_style));
         }
